291f813dbd4ec5ad3eaa79a47a70abb954b58233
stds58 <stds58@gmail.com>
2024-11-27 21:46:09 +0300

    gigachat test

--- fic/gigachat/xx.py
+++ fic/gigachat/xx.py
@@ -2,12 +2,13 @@ from gigachat import GigaChat
 
 TOKEN_GIGACHAT = 'NTViNzk2ZTYtM2M1Mi00MGQxLWI4MGUtOTMwMjhhNGZhMDVhOjA3NTMwZWRiLWU0MDktNDFmZS04MzNiLWEyY2U1OTgxNDhlMg=='
 
+tekst = """распознай поля ввода из этого текста и сделай из этого словарь json- 
+    паспорт 4545033402 фамилия ыаваыпп имя ыапа отчество впрпрапр
+    """
 
 
 with GigaChat(credentials=TOKEN_GIGACHAT, scope="GIGACHAT_API_PERS", verify_ssl_certs=False) as giga:
-    response = giga.chat("""распознай поля ввода из этого текста и сделай из этого словарь json- 
-    паспорт 4545033402 фамилия ыаваыпп имя ыапа отчество впрпрапр
-    """)
+    response = giga.chat(tekst)
     print(response.choices[0].message.content)
 
 
